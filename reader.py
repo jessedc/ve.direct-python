551dbcf2c1f04c1de28ccbfd8c7e0d1384298ad1
Jesse Collis <jesse@jcmultimedia.com.au>
2019-01-26 22:46:42 +1100

    More victron mapping
    Starting point for filtering out duplicate values

--- reader.py
+++ reader.py
@@ -5,12 +5,12 @@
 from vedirect import Vedirect
 import paho.mqtt.client as mqtt
 
-
 mqttc = mqtt.Client('319a-victron-mppt')
 mqttc.username_pw_set('iot-user', 'avrdragon')
+broker_ip = '192.168.1.15'
 
-base_topic = 'victron-mppt/'
-# last_victron_data = None
+base_topic = 'victron/mppt/'
+
 victron_key_map = {
     'LOAD': 'now/battery-state',
     'H19': 'total/yield',
@@ -40,20 +40,56 @@ victron_state = {
     '5': 'Float'
 }
 
+mppt_state = {
+    '0': 'Off',
+    '1': 'Limited',
+    '2': 'Active'
+}
+
+victron_error = {
+    '0': 'No error [error 0]',
+    '2': 'Battery voltage too high [error 2]',
+    '17': 'Charger temperature too high [error 17]',
+    '18': 'Charger over current [error 18]',
+    '19': 'Charger current reversed [error 19]',
+    '20': 'Bulk time limit exceeded [error 20]',
+    '21': 'Current sensor issue [error 21]',
+    '26': 'Terminals overheated [error 26]',
+    '33': 'Input voltage too high (solar panel) [error 33]',
+    '34': 'Input current too high (solar panel) [error 34]',
+    '38': 'Input shutdown (excessive battery voltage) [error 38]',
+    '116': 'Factory calibration data lost [error 116]',
+    '117': 'Invalid/incompatible firmware [error 117]',
+    '119': 'User settings invalid [error 119]'
+}
+
+# Local variable to store previous frame
+# previous_victron_frame = {}
+
 
 def on_victron_data_callback(data):
+    # global previous_victron_frame
+
     for key, value in data.iteritems():
+        # NB: To filter values, do something like this:
+        # if previous_victron_frame.get(key, None) == value:
+        #     continue
+        # else:
+        #     previous_victron_frame[key] = value
+
         if key == 'CS':
             value = victron_state[value]
+        elif key == 'MPPT':
+            value = mppt_state[value]
+        elif key == 'ERR':
+            value = victron_error[value]
 
-        key = victron_key_map[key]
-        mqttc.publish(base_topic + key, value)
+        key = base_topic + victron_key_map[key]
+        mqttc.publish(key, value)
 
 
-mqttc.connect('10.0.1.212')
+mqttc.connect(broker_ip)
 mqttc.loop_start()
 
 ve = Vedirect('/dev/ttyAMA0', 1)
 ve.read_data_callback(on_victron_data_callback)
-
-
